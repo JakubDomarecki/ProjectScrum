2b9012fc2390549b9469f622632fecc569472de7
Artystycznepaznokcie <bartlomiej.sokalski.job@gmail.com>
2024-04-23 11:26:58 +0200

    BS/02/mainRecipes

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
+const dodajPrzepis = document.querySelector('#left')
+const dodajPlan = document.querySelector('#right')
+const pokazPrzepis = document.querySelector('#przepis')
+const pokazPlan = document.querySelector('#plan')
+dodajPrzepis.addEventListener('click',function (event) {
+
+pokazPrzepis.classList.toggle('ukrytePrzepis')
+} )
+
+
+
+
+
+
+dodajPlan.addEventListener('click', function (event) {
+pokazPlan.classList.toggle('ukrytePlan')
+})
 
